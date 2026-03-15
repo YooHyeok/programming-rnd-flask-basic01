98a625e91a776098faa6f5520fa9d6d20a2ad1e0
YooHyeok <yjk7454@naver.com>
2026-03-15 21:57:17 +0900

    feat: Flask Get방식 요청 파라미터 전달 - query string 예제

--- python_flask/app.py
+++ python_flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 app = Flask(__name__)
 
 @app.route('/')
@@ -12,6 +12,14 @@ def show_user_profile(username):
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'Post %d' % post_id
+
+@app.route("/search")
+def show_search():
+    args_dict = request.args.to_dict()
+    print(request.args['word'])
+    # print(request.args.word) # MultiDict 객체이므로 지원하지 않음
+    print(request.args.get('num'))
+    return args_dict
 
 if __name__ == '__main__':
     app.run()
